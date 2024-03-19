d63527efe6cc00c1a54a9af134c39379fbbe695d
publicresources <155874174+publicresources@users.noreply.github.com>
2024-03-18 23:10:04 -0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,40 +1,38 @@
-{
-    let getPackageName = null
-    {
-        let cachedPackageName = null;
-        getPackageName = function () {
-            if (cachedPackageName !== null) {
-                return cachedPackageName;
-            }
-            const fopenAddr = Module.findExportByName(null, "fopen");
-            const fopen = new NativeFunction(fopenAddr, "pointer", ["pointer", "pointer"]);
-            const FILE = fopen(Memory.allocUtf8String("/proc/self/cmdline"), Memory.allocUtf8String("r"));
-            if (!FILE) {
-                return "Error";
-            }
-            const fgetsAddr = Module.findExportByName(null, "fgets");
-            const fgets = new NativeFunction(fgetsAddr, "int32", ["pointer", "int32", "pointer"]);
-            const buffer = Memory.alloc(255);
-            fgets(buffer, 255, FILE);
-            const fcloseAddr = Module.findExportByName(null, "fclose");
-            const fclose = new NativeFunction(fcloseAddr, "int32", ["pointer"]);
-            fclose(FILE);
+let getPackageName = null
 
-            // Cache in memory
-            cachedPackageName = buffer.readCString();
+{
+    let cachedPackageName = null;
+    getPackageName = function () {
+        if (cachedPackageName !== null) {
             return cachedPackageName;
         }
+        const fopenAddr = Module.findExportByName(null, "fopen");
+        const fopen = new NativeFunction(fopenAddr, "pointer", ["pointer", "pointer"]);
+        const FILE = fopen(Memory.allocUtf8String("/proc/self/cmdline"), Memory.allocUtf8String("r"));
+        if (!FILE) {
+            return "Error";
+        }
+        const fgetsAddr = Module.findExportByName(null, "fgets");
+        const fgets = new NativeFunction(fgetsAddr, "int32", ["pointer", "int32", "pointer"]);
+        const buffer = Memory.alloc(255);
+        fgets(buffer, 255, FILE);
+        const fcloseAddr = Module.findExportByName(null, "fclose");
+        const fclose = new NativeFunction(fcloseAddr, "int32", ["pointer"]);
+        fclose(FILE);
+
+        // Cache in memory
+        cachedPackageName = buffer.readCString();
+        return cachedPackageName;
     }
+}
 
 
-    if (true) {//) {
-        Java.perform(function () {
-            Java.scheduleOnMainThread(function () {
-                var toast = Java.use("android.widget.Toast");
-                const pkg = getPackageName()
-                toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new(pkg), 1).show();
-            });
+if (true){//getPackageName().startsWith("com.")) {
+    Java.perform(function () {
+        Java.scheduleOnMainThread(function () {
+            var toast = Java.use("android.widget.Toast");
+            const pkg = getPackageName()
+            toast.makeText(Java.use("android.app.ActivityThread").currentApplication().getApplicationContext(), Java.use("java.lang.String").$new(pkg), 1).show();
         });
-    }
-
+    });
 }
